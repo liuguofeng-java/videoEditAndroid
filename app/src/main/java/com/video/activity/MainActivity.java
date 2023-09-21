5f57d384bad00e079fc6ee6e693a0259e4109a50
liuguofeng <liuguofeng-java@qq.com>
2023-09-21 09:58:00 +0800

    初始化项目

---
+++ app/src/main/java/com/video/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.video.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.video.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
